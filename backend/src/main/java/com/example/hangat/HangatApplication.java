d7b972c26e892a844605a90692a4571242774962
jdh <wjdehd555@gmail.com>
2026-08-19 22:59:43 +0900

    [Feat] 백엔드 Gradle 프로젝트 초기화

---
+++ backend/src/main/java/com/example/hangat/HangatApplication.java
@@ -0,0 +1,12 @@
+package com.example.hangat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangatApplication.class, args);
+    }
+}
